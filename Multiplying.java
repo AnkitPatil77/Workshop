2d0ebbeecb7ea48e438a40b0f79f9cb244ae9cf7
unknown <patil.ankitpatil.ankit@gmail.com>
2022-07-14 19:33:37 +0530

    Mutiplyfloatnumber

---
+++ Multiplying.java
@@ -0,0 +1,15 @@
+
+public class Multiplying {
+ 
+	public static void main(String[] args) {
+		
+		double first = 6;
+		double second = 9.6f;
+		
+		double product = first * second;
+		
+		System.out.println("The product is: " + product);
+		
+		
+	}
+}
